a3545475362a6792a907dab65b9e57e9a2ee587a
catthug92 <itnikitins@gmail.com>
2023-07-06 12:14:15 +0300

    start

---
+++ NetworkingPOST/NetworkingPOSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingPOSTApp.swift
+//  NetworkingPOST
+//
+//  Created by Никитин Артем on 6.07.23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingPOSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
